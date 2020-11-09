16c1d98a023438cb86c83c475cdd73f7dd04a8ac
Art <m_a_s@bk.ru>
2020-11-09 12:13:17 +0300

    Lesson2

--- app/src/main/java/com/mas/material/ui/home/HomeFragment.kt
+++ app/src/main/java/com/mas/material/ui/home/HomeFragment.kt
@@ -1,13 +1,16 @@
 package com.mas.material.ui.home
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.mas.material.R
 
 class HomeFragment : Fragment() {
@@ -22,10 +25,28 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+        val textInputLayout: TextInputLayout = root.findViewById(R.id.textField)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+            textInputLayout.helperText = it
         })
+
+        val passtext: TextInputEditText = root.findViewById(R.id.pass_text)
+        passtext.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s != null)
+                    if (s.length > textInputLayout.counterMaxLength) {
+                        textInputLayout.error = "Max count!!!"
+                    } else {
+                        textInputLayout.error = null
+                    }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+
+        })
+
         return root
     }
 }
