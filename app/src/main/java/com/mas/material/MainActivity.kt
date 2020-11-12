1d3ec09c3d9f4c8c441b4ad655a7c10520c7b857
Art <m_a_s@bk.ru>
2020-11-12 12:34:14 +0300

    Lesson3

--- app/src/main/java/com/mas/material/MainActivity.kt
+++ app/src/main/java/com/mas/material/MainActivity.kt
@@ -2,6 +2,7 @@ package com.mas.material
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
